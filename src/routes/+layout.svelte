fdf6513a65c1dcdc7cb3b90722d091473370177e
UrielCuriel <urielcuriel@outlook.com>
2023-01-27 14:28:11 -0600

    chore: release 1.0.1
    Release-As: 1.0.1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import Nav from '$components/nav.svelte';
 	import '../app.css';
 </script>
 
 <main class="bg-neutral-100 min-h-screen pb-4 text-neutral-500">
+	<Nav />
 	<slot />
 </main>
